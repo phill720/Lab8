38e983892608cf4e6cc554d904e2dfcc6613c176
phill720 <phillipschris902@gmail.com>
2016-09-07 16:10:02 -0400

    Lab 8 Batting average

--- Lab8/Program.cs
+++ Lab8/Program.cs
@@ -4,12 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab8
+namespace Lab8_batting
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Console.Write("Welcome to Batting Average Calculator!\nEnter number of times at bat:\t");
+            int BatTimes = int.Parse(Console.ReadLine());
+            Console.WriteLine("0 = out, 1 = single, 2 = double, 3 = triple, 4 = home run");
+            int slug = 0;
+            int makes = 0;
+            for (int count = 0; count < BatTimes; count++)
+            {
+                Console.Write($"Result for at-bat {count}:\t");
+                int res = int.Parse(Console.ReadLine());
+                slug = slug + res;
+                if (res >= 1)
+                {
+                    makes++;
+                }
+            }
+            Console.WriteLine($"\nBatting avg:\t\t{BatAverage(BatTimes, makes)}");
+            Console.WriteLine($"Slugging percentage:\t{SlugPercent(slug, BatTimes)}");
+        }
+        public static double SlugPercent(double slug, double BatTimes)
+        {
+            double slugP = slug / BatTimes;
+            return Math.Round(slugP,4);
+        }
+        public static double BatAverage(double BatTimes, double makes)
+        {
+            double BatAvg = makes / BatTimes;
+            return Math.Round(BatAvg,4);
         }
     }
-}
+} 
---
+++ Lab8Test/AverageTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab8_batting;
+
+namespace Lab8Test
+{
+    [TestClass]
+    public class AverageTests
+    {
+        [TestMethod]
+        public void BattingPercentageIs0WhenNoRuns()
+        {
+            int[] BattingSheet = new int[5];
+            BattingSheet[0] = 0;
+            BattingSheet[1] = 0;
+            BattingSheet[2] = 0;
+            BattingSheet[4] = 0;
+            BattingSheet[5] = 0;
+            //double average = Program.;
+        }
+        [TestMethod]
+        public void TestBattingPercentageis60()
+        {
+            //int[] battingSheet = new int[5];
+            //battingSheet[0] = 0;
+            //battingSheet[1] = 0;
+            //battingSheet[2] = 2;
+            //battingSheet[3] = 4;
+            //battingSheet[4] = 1;
+            double average = Program.BatAverage(5,3);
+            Assert.AreEqual(0.6, average);
+        }
+    }
+}
