63dda6bbff14b93208214cee5a43ef34dc049b8f
phill720 <phillipschris902@gmail.com>
2016-09-21 13:42:32 -0400

    Batting Average
    Minor code cleanup

--- Lab8/Program.cs
+++ Lab8/Program.cs
@@ -3,7 +3,7 @@
 namespace Lab8_batting
 {
     public class Program
-    {
+    {                   
         public static void Main(string[] args)
         {
             Console.Write("Welcome to Batting Average Calculator!\nEnter number of times at bat:\t");
@@ -11,6 +11,7 @@ namespace Lab8_batting
             Console.WriteLine("\nEnter 'OUT', 'SINGLE', 'DOUBLE', 'TRIPLE', or 'HOMERUN' \n");
             int slug = 0;
             int makes = 0;
+            //---"What did they get?:" (Answer)---
             for (int count = 0; count < BatTimes; count++)
             {
                 Console.Write($"Result for at-bat {count}:\t");
@@ -24,15 +25,17 @@ namespace Lab8_batting
             Console.WriteLine($"\nBatting avg:\t\t{BatAverage(BatTimes, makes)}");
             Console.WriteLine($"Slugging percentage:\t{SlugPercent(slug, BatTimes)}");
         }
+        //------Slugging Percentage------
         public static double SlugPercent(double slug, double BatTimes)
         {
             double slugP = slug / BatTimes;
-            return Math.Round(slugP,4);
+            return Math.Round(slugP, 4);
         }
+        //------Batting Average------
         public static double BatAverage(double BatTimes, double makes)
         {
             double BatAvg = makes / BatTimes;
-            return Math.Round(BatAvg,4);
+            return Math.Round(BatAvg, 4);
         }
     }
-} 
+}
